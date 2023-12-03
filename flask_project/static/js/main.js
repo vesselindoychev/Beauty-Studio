8207461fa139372401c86d624ecba5ebdbf17614
Veselin Doychev <veselindoi123@gmail.com>
2023-12-03 16:14:07 +0200

    Add main.js file, where created on click event to hide and show location for out salon

---
+++ flask_project/static/js/main.js
@@ -0,0 +1,34 @@
+locationBtn = document.querySelector('.location-dropdown');
+locationBtnIcon = document.querySelector('.location-dropdown i');
+locationBtnAngleIcon = document.querySelector('.location-dropdown #angle');
+
+locationOpt = document.querySelector('.location-options');
+
+let counter = 0
+
+function onClick(event) {
+    if (event.target === locationBtn) {
+        counter += 1
+        if (counter === 1) {
+            locationOpt.removeAttribute('hidden')
+            locationBtnIcon.style.color = '#c48d4d';
+            locationBtnAngleIcon.removeAttribute('class')
+            locationBtnAngleIcon.setAttribute('class', 'fa-solid fa-angle-right')
+        } else if (counter === 2) {
+            locationOpt.setAttribute('hidden', '')
+            locationBtnIcon.style.color = 'black';
+            locationBtnAngleIcon.removeAttribute('class')
+            locationBtnAngleIcon.setAttribute('class', 'fa-solid fa-angle-down')
+            counter = 0
+        }
+    } else {
+        locationOpt.setAttribute('hidden', '')
+        counter = 0
+        locationBtnAngleIcon.removeAttribute('class')
+        locationBtnAngleIcon.setAttribute('class', 'fa-solid fa-angle-down')
+        locationBtnIcon.style.color = 'black';
+    }
+}
+
+document.addEventListener('click', onClick)
+
